4018e1a21201a34e9fbd11472f2aefeb728ca21c
Ezerom77 <fullstack@ezerom.com.ar>
2024-11-28 11:36:46 -0300

    analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   // used to generate images
@@ -12,7 +14,16 @@ export default defineConfig({
         : // : "https://localhost:3000/",
           "https://victimasdelalgoritmo.com.ar/",
   trailingSlash: "ignore",
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [
+    sitemap(),
+    UnoCSS({ injectReset: true }),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
